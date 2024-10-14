63c9e13cb1cd0ec4ef0f99296cb74c9d9d425f2a
rstoyanchev <rossen.stoyanchev@broadcom.com>
2024-10-14 18:13:43 +0100

    Update processPath for double encoding

--- spring-webmvc/src/main/java/org/springframework/web/servlet/resource/ResourceHttpRequestHandler.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/resource/ResourceHttpRequestHandler.java
@@ -597,18 +597,26 @@ public class ResourceHttpRequestHandler extends WebContentGenerator
 	}
 
 	private static String normalizePath(String path) {
-		if (path.contains("%")) {
-			try {
-				path = URLDecoder.decode(path, "UTF-8");
+		String result = path;
+		if (result.contains("%")) {
+			result = decode(result);
+			if (result.contains("%")) {
+				result = decode(result);
 			}
-			catch (Exception ex) {
-				return "";
-			}
-			if (path.contains("../")) {
-				path = StringUtils.cleanPath(path);
+			if (result.contains("../")) {
+				return StringUtils.cleanPath(result);
 			}
 		}
 		return path;
+	}
+
+	private static String decode(String path) {
+		try {
+			return URLDecoder.decode(path, "UTF-8");
+		}
+		catch (Exception ex) {
+			return "";
+		}
 	}
 
 	/**

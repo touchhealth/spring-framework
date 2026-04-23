d55079852b7bbae103d8a9f121ac5f29674b2903
Brian Clozel <brian.clozel@broadcom.com>
2026-04-23 15:04:02 +0200

    Enfoce single version removal in content versioning
    Prior to this commit, content based version strategies would remove all
    instances of the version string in the request path when trying to
    resolve the original resource with the chain.
    This can cause issues in rare cases where there is a collision between
    the content version and some other version string in the request path.
    
    Because this strategy is based on the contents of the file itself, we
    should only remove the last instance of the version string and then
    attempt to resolve the original file.

--- spring-webflux/src/test/java/org/springframework/web/reactive/resource/ContentBasedVersionStrategyTests.java
+++ spring-webflux/src/test/java/org/springframework/web/reactive/resource/ContentBasedVersionStrategyTests.java
@@ -62,6 +62,14 @@ public class ContentBasedVersionStrategyTests {
 	}
 
 	@Test
+	public void removeVersionOnlyOnce() {
+		String hash = "sha";
+		String path = "font-awesome/css%s%s/font-awesome.min%s%s.css";
+
+		assertThat(this.strategy.removeVersion(String.format(path, "-", hash, "-", hash), hash)).isEqualTo(String.format(path, "-", hash, "", ""));
+	}
+
+	@Test
 	public void getResourceVersion() throws Exception {
 		Resource expected = new ClassPathResource("test/bar.css", getClass());
 		String hash = DigestUtils.md5DigestAsHex(FileCopyUtils.copyToByteArray(expected.getInputStream()));
